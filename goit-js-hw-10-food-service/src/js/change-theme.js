f289b0ba1381ffffef902e6787cb0a29fef88480
Roman27011986 <>
2020-12-16 16:11:23 +0200

    fix

--- goit-js-hw-10-food-service/src/js/change-theme.js
+++ goit-js-hw-10-food-service/src/js/change-theme.js
@@ -8,20 +8,26 @@ inputRef.addEventListener('change', changeTheme)
 
 function changeTheme(event) {
     const checkRef = event.target.checked
-    
+     
     if (checkRef) {
         localStorage.setItem('theme',theme.DARK )
         document.body.classList.remove('light-theme')
         document.body.classList.add('dark-theme') 
         inputRef.checked = true
+        
     } 
     if (!checkRef) {
         localStorage.removeItem('theme', theme.DARK)
         localStorage.setItem('theme',theme.LIGHT)
         document.body.classList.remove('dark-theme') 
         document.body.classList.add('light-theme')
-    }  
+         
+    } 
+    
+   
 }
+
+
 
 
 if (savedDarkTheme === theme.DARK) {
